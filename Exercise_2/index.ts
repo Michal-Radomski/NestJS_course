96f60679eaba0453b267b7613dbcd3de3a881c74
Michal Radomski <michalra@op.pl>
2022-05-10 22:58:53 +0200

    OOP exercise done

--- Exercise_2/index.ts
+++ Exercise_2/index.ts
@@ -1,12 +1,12 @@
-//* Imperative programming
+//* Imperative Programming
 
-interface Shopping {
+interface Item {
   name: string;
   count: number;
   price: number;
 }
 
-const basket: Shopping[] = [];
+const basket: Item[] = [];
 
 basket.push({name: "bread", count: 1, price: 2.5});
 
@@ -22,3 +22,35 @@ for (let i = 0; i < basket.length; i++) {
 
 console.log("names:", names);
 console.log("sum:", sum);
+
+//* Object Oriented Programming
+
+class Basket {
+  items: any[];
+
+  constructor() {
+    this.items = [];
+  }
+  add(name: string, price: number, count: number = 1) {
+    this.items.push({
+      name: name,
+      price: price,
+      count: count,
+    });
+  }
+  names2() {
+    return this.items.map((item) => item.name);
+  }
+  sum2() {
+    return this.items.reduce((prev, current) => prev + current.price * current.count, 0);
+  }
+}
+
+const basket2 = new Basket();
+// console.log("basket2:", basket2);
+basket2.add("bread", 1, 2.5);
+basket2.add("cucumber", 5, 4);
+
+console.log("basket2:", basket2);
+console.log("basket2.names2():", basket2.names2());
+console.log("basket2.sum2():", basket2.sum2());
