ec850979e96cb9b5e0a9d16d20230a9672d01daf
Michal Radomski <michalra@op.pl>
2022-05-11 00:34:19 +0200

    Nest - initial exercise

--- Projekt_1/src/app.module.ts
+++ Projekt_1/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TestowyModule } from './testowy/testowy.module';
 
 @Module({
-  imports: [],
+  imports: [TestowyModule],
   controllers: [AppController],
   providers: [AppService],
 })
