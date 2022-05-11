45735c00776d1a612e7384b0420a6759e5daa2c0
Michal Radomski <michalra@op.pl>
2022-05-12 00:21:21 +0200

    Asynchronicznosc - cwiczenie

--- Exercise_3 TS/index5.ts
+++ Exercise_3 TS/index5.ts
@@ -21,3 +21,69 @@ console.log("sum3:", sum3(3, 4));
   const test = new Test();
   console.log(test.someMethod());
 }
+
+{
+  //* Asynchroniczność
+  //- Metoda 1
+  // function goToPKP(clb: () => void) {
+  //   setTimeout(clb, 1000);
+  // }
+  // function waitForTrain(clb: () => void) {
+  //   setTimeout(clb, 1500);
+  // }
+  // function travelToDestination(clb: () => void) {
+  //   setTimeout(clb, 2000);
+  // }
+
+  // goToPKP(() => {
+  //   console.log("Dodarłem do PKP");
+  //   waitForTrain(() => {
+  //     console.log("Pociąg przyjechał");
+  //     travelToDestination(() => {
+  //       console.log("Dojechałem");
+  //     });
+  //   });
+  // });
+
+  //- Metoda 2
+  // function goToPKP(): Promise<void> {
+  //   return new Promise((resolve, _reject) => setTimeout(resolve, 1000));
+  // }
+  // function waitForTrain(): Promise<void> {
+  //   return new Promise((resolve, _reject) => setTimeout(resolve, 1500));
+  // }
+  // function travelToDestination(): Promise<void> {
+  //   return new Promise((resolve, _reject) => setTimeout(resolve, 2000));
+  // }
+
+  // goToPKP().then(() => {
+  //   console.log("Dotarłem do PKP");
+  //   waitForTrain().then(() => {
+  //     console.log("Pociąg przyjechał");
+  //     travelToDestination().then(() => {
+  //       console.log("Dojechałem");
+  //     });
+  //   });
+  // });
+
+  //- Metoda 3
+
+  function goToPKP(): Promise<void> {
+    return new Promise((resolve, _reject) => setTimeout(resolve, 1000));
+  }
+  function waitForTrain(): Promise<void> {
+    return new Promise((resolve, _reject) => setTimeout(resolve, 1500));
+  }
+  function travelToDestination(): Promise<void> {
+    return new Promise((resolve, _reject) => setTimeout(resolve, 2000));
+  }
+
+  (async () => {
+    await goToPKP();
+    console.log("Dotarłem do PKP");
+    await waitForTrain();
+    console.log("Pociąg przyjechał");
+    await travelToDestination();
+    console.log("Dojechałem");
+  })();
+}
