9c1885e94e03d1bf6e3b3489e418965cda3b0c2b
Michal Radomski <michalra@op.pl>
2022-05-18 21:48:40 +0200

    shop-item.entity.ts added

--- Projekt_2/src/app.module.ts
+++ Projekt_2/src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
@@ -11,7 +12,7 @@ import { ShopModule } from './shop/shop.module';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [BasketModule, ShopModule, UsersModule],
+  imports: [BasketModule, ShopModule, UsersModule, TypeOrmModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ Projekt_2/src/shop/shop-item.entity.ts
@@ -0,0 +1,29 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class ShopItem {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({
+    length: 60,
+  })
+  // name: string | null;
+  name: string;
+
+  @Column({
+    // type: "text",
+    length: 1000,
+    default: '(brak opisu)',
+    // default: null,
+    // nullable: true,
+  })
+  description: string;
+
+  @Column({
+    type: 'float',
+    precision: 6,
+    scale: 2,
+  })
+  price: number;
+}
