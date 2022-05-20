f24480469b376278a05691dc32a65c21ee6eaaf7
Michal Radomski <michalra@op.pl>
2022-05-21 01:05:40 +0200

    Projekt_2A - initial commit2

--- Projekt_2A/src/app.module.ts
+++ Projekt_2A/src/app.module.ts
@@ -3,16 +3,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
-// import { ShopController } from './shop/shop.controller';
-// import { ShopService } from './shop/shop.service';
-// import { BasketController } from './basket/basket.controller';
-// import { BasketService } from './basket/basket.service';
 import { BasketModule } from './basket/basket.module';
 import { ShopModule } from './shop/shop.module';
-import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [BasketModule, ShopModule, UsersModule, TypeOrmModule.forRoot()],
+  imports: [BasketModule, ShopModule, TypeOrmModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
--- Projekt_2A/src/users/users.controller.ts
+++
@@ -1,9 +0,0 @@
-import { Controller, Get } from '@nestjs/common';
-
-@Controller('/users')
-export class UsersController {
-  @Get()
-  getHello(): string {
-    return "<div style='color:green'>Users</div>";
-  }
-}
