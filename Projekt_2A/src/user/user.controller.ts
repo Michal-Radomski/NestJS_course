c1468223afdbacd8210c331525a0e63f561bb162
Michal Radomski <michalra@op.pl>
2022-05-21 20:42:20 +0200

    User module done

--- Projekt_2A/src/user/user.controller.ts
+++ Projekt_2A/src/user/user.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Inject, Post } from '@nestjs/common';
+import { RegisterUserResponse } from 'src/interfaces/user';
+import { RegisterDto } from './dto/register.dto';
+import { UserService } from './user.service';
 
-@Controller('user')
-export class UserController {}
+@Controller('/user')
+export class UserController {
+  constructor(@Inject(UserService) private userService: UserService) {}
+
+  @Post('/register')
+  register(@Body() newUser: RegisterDto): Promise<RegisterUserResponse> {
+    return this.userService.register(newUser);
+  }
+}
