31c51dacda8ff42169058a8371ca6dbe51257803
Michal Radomski <michalra@op.pl>
2022-05-21 17:47:03 +0200

    temp commit1

--- Projekt_2A/src/basket/basket.service.ts
+++ Projekt_2A/src/basket/basket.service.ts
@@ -1,16 +1,14 @@
 import { forwardRef, Inject, Injectable } from '@nestjs/common';
 import {
   AddToBasketResponse,
-  GetBasketResponse,
   RemoveFromBasketResponse,
 } from 'src/interfaces/basket';
 import { ShopService } from 'src/shop/shop.service';
 import { AddItemDto } from './dto/add-item.dto';
+import { ItemInBasket } from './item-in-basket.entity';
 
 @Injectable()
 export class BasketService {
-  private items: AddItemDto[] = [];
-
   constructor(
     @Inject(forwardRef(() => ShopService)) private shopService: ShopService,
   ) {}
@@ -25,29 +23,38 @@ export class BasketService {
       count < 1 ||
       !(await this.shopService.hasItem(name))
     ) {
+      const item = new ItemInBasket();
+      item.name = name;
+      item.count = count;
+
+      await item.save();
+
       return {
-        isSuccess: false,
+        isSuccess: true,
+        id: item.id,
       };
     }
   }
 
-  remove(number: number): RemoveFromBasketResponse {
-    if (this.items[number]) {
-      this.items.splice(number, 1);
+  async remove(id: string): Promise<RemoveFromBasketResponse> {
+    const item = await ItemInBasket.findOne(id);
 
+    if (item) {
+      await item.remove();
       return { isSuccess: true };
     }
-
     return { isSuccess: false };
   }
-  getAll(): GetBasketResponse {
-    return this.items;
+  async getAll(): Promise<ItemInBasket[]> {
+    return ItemInBasket.find();
   }
 
   async getTotalPrice(): Promise<number> {
+    const items = await this.getAll();
+
     return (
       await Promise.all(
-        this.items.map(
+        items.map(
           async (item) =>
             (await this.shopService.getPrice(item.name)) * item.count * 1.23,
         ),
