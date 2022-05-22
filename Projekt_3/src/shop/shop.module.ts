0f61fd94bfdef015d0e77c2891867558446c25f2
Michal Radomski <michalra@op.pl>
2022-05-22 18:09:42 +0200

    MongoDB was configured with the app

--- Projekt_3/src/shop/shop.module.ts
+++ Projekt_3/src/shop/shop.module.ts
@@ -1,7 +1,17 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ItemShop, ItemShopSchema } from 'src/interfaces/shop-item.schema';
 import { ShopService } from './shop.service';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: ItemShop.name,
+        schema: ItemShopSchema,
+      },
+    ]),
+  ],
   providers: [ShopService],
 })
 export class ShopModule {}
--- Projekt_3/src/shop/shop.service.ts
+++ Projekt_3/src/shop/shop.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { ItemShop } from 'src/interfaces/shop-item.schema';
 
 @Injectable()
-export class ShopService {}
+export class ShopService {
+  constructor(
+    @InjectModel(ItemShop.name) private itemShopModel: Model<ItemShop>,
+  ) {}
+}
