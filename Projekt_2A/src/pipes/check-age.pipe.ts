09532f8c0f30597b531367a8312f1cde902d44c1
Michal Radomski <michalra@op.pl>
2022-05-23 01:06:16 +0200

    custom pipe added

---
+++ Projekt_2A/src/pipes/check-age.pipe.ts
@@ -0,0 +1,27 @@
+import {
+  PipeTransform,
+  Injectable,
+  ArgumentMetadata,
+  BadRequestException,
+} from '@nestjs/common';
+
+interface CheckAgePipeOptions {
+  minAge: number;
+}
+
+@Injectable()
+export class CheckAgePipe implements PipeTransform {
+  constructor(private options: CheckAgePipeOptions) {}
+
+  transform(value: string, _metadata: ArgumentMetadata): number {
+    const age = Number(value);
+
+    if (Number.isNaN(age) || age < this.options.minAge) {
+      throw new BadRequestException(
+        `Age must be numeric string and min. ${this.options.minAge} years old.`,
+      );
+    }
+
+    return Number(value);
+  }
+}
--- Projekt_2A/src/shop/shop.controller.ts
+++ Projekt_2A/src/shop/shop.controller.ts
@@ -7,6 +7,7 @@ import {
   ParseIntPipe,
 } from '@nestjs/common';
 import { ShopItemInterface } from 'src/interfaces/shop';
+import { CheckAgePipe } from 'src/pipes/check-age.pipe';
 import { ShopService } from './shop.service';
 
 @Controller('/shop')
@@ -24,6 +25,19 @@ export class ShopController {
     index?: number,
   ) {
     console.log('index:', index);
+    return null;
+  }
+  @Get('/test2/:age')
+  testAge(
+    @Param(
+      'age',
+      new CheckAgePipe({
+        minAge: 21,
+      }),
+    )
+    age?: number,
+  ) {
+    console.log('age:', age, typeof age);
     return null;
   }
 }
