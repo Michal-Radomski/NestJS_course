f0c73f018595488d352815d9a0e5a0ab4dc0d68e
Michal Radomski <michalra@op.pl>
2022-05-23 17:56:07 +0200

    helmet was added

--- Projekt_2A/src/main.ts
+++ Projekt_2A/src/main.ts
@@ -2,9 +2,16 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ImATeapotExceptionFilter } from './filters/global-exception.filter';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import helmet from 'helmet';
+
+// import * as Helmet from 'helmet';
+// console.log('helmet:', helmet);
+// console.log('Helmet:', Helmet);
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  (app as NestExpressApplication).use(helmet());
   app.enableShutdownHooks();
   app.useGlobalFilters(new ImATeapotExceptionFilter());
   app.useGlobalPipes(
