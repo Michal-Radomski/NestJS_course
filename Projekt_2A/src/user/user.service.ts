a581fcb6d36569c829fcd86abb29140e7f72f098
Michal Radomski <michalra@op.pl>
2022-05-25 02:34:03 +0200

    nestjs-console was added, app and course done

--- Projekt_2A/src/user/user.service.ts
+++ Projekt_2A/src/user/user.service.ts
@@ -3,8 +3,13 @@ import { RegisterUserResponse } from 'src/interfaces/user';
 import { hashPwd } from 'src/utils/hash-pwd';
 import { RegisterDto } from './dto/register.dto';
 import { User } from './user.entity';
+import { Command, Console } from 'nestjs-console';
 
 @Injectable()
+@Console({
+  // @ts-ignore
+  name: 'users',
+})
 export class UserService {
   filter(user: User): RegisterUserResponse {
     const { id, email } = user;
@@ -17,10 +22,31 @@ export class UserService {
     user.pwdHash = hashPwd(newUser.pwd);
     await user.save();
 
-    return this.filter(user);
+    return user;
   }
 
   async getOneUser(userId: string): Promise<User> {
     return await User.findOne(userId);
+  }
+
+  @Command({
+    command: 'list',
+    description: 'List all of the users',
+  })
+  async listUsersCmd() {
+    console.log((await User.find()).map(this.filter));
+  }
+
+  @Command({
+    command: 'add <email> <pwd>',
+    description: 'Add new user',
+  })
+  async addUsersCmd(email: string, pwd: string) {
+    console.log(
+      await this.register({
+        email,
+        pwd,
+      }),
+    );
   }
 }
